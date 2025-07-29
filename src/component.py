keboola/component-bigquery-byodb-view-writer

commit 728ec6166a8d1ba608aef45c91cbea79d8fa268f

Refactor BigqueryClient initialization and remove location parameter

src/component.py
@@ -24,8 +24,11 @@ KEY_COLUMNS = 'columns'
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_SERVICE_ACCOUNT, KEY_SOURCE_PROJECT_ID,
-                       KEY_DESTINATION_PROJECT_ID, KEY_DESTINATION_DATASET_ID, KEY_DESTINATION_VIEW_NAME]
+REQUIRED_PARAMETERS = [KEY_SERVICE_ACCOUNT,
+                       KEY_SOURCE_PROJECT_ID,
+                       KEY_DESTINATION_PROJECT_ID,
+                       KEY_DESTINATION_DATASET_ID,
+                       KEY_DESTINATION_VIEW_NAME]
 
 SCOPES = ['https://www.googleapis.com/auth/bigquery']
 
@@ -43,7 +46,6 @@ class Component(ComponentBase):
 
     def __init__(self):
         super().__init__()
-        self.location = None
 
     @staticmethod
     def validate_credentials(parameters):
@@ -67,8 +69,6 @@ class Component(ComponentBase):
         return parameters
 
     def get_bigquery_credentials(self):
-        self.location = self.configuration.config_data.get('image_parameters', {}).get('location') or 'EU'
-
         credentials = (self.configuration.config_data.get('image_parameters', {}).get(KEY_SERVICE_ACCOUNT)
                        or self.configuration.parameters.get(KEY_SERVICE_ACCOUNT))
 
@@ -94,7 +94,7 @@ class Component(ComponentBase):
             raise UserException('Another project storage backend is not supported!')
 
         # create bigquery client
-        bg = BigqueryClient(self.get_bigquery_credentials(), self.location)
+        bg = BigqueryClient(self.get_bigquery_credentials())
 
         # get destination datasets
         destination_dataset = bg.find_dataset(params.get(KEY_DESTINATION_PROJECT_ID),
@@ -121,7 +121,10 @@ class Component(ComponentBase):
                 logging.info(f"Source and destination datasets are in the same location: {source_dataset.location}")
 
             # create view
-            bg.create_view(destination_dataset, source_dataset, source_table, source_table_columns_descriptions,
+            bg.create_view(destination_dataset,
+                           source_dataset,
+                           source_table,
+                           source_table_columns_descriptions,
                            custom_columns,
                            params.get(KEY_DESTINATION_VIEW_NAME))
         else:

src/google_cloud/bigquery_client.py
@@ -13,8 +13,8 @@ class BigqueryClient:
             scopes=scopes
         )
 
-    def __init__(self, credentials, location):
-        self.client = bigquery.Client(credentials=credentials, location=location)
+    def __init__(self, credentials):
+        self.client = bigquery.Client(credentials=credentials)
 
     def _update_access(self, source_dataset, view):
         access_entries = source_dataset.access_entries

commit ca4aa307418762bf56db608d121a65b54598c2d5

location argument removed also from sync actions
this fixes a sync action bug introduced in 728ec61

src/component.py
@@ -209,7 +209,7 @@ class Component(ComponentBase):
         Returns:
 
         """
-        bq = BigqueryClient(credentials=self.get_bigquery_credentials(), location=self.location)
+        bq = BigqueryClient(credentials=self.get_bigquery_credentials())
         projects = bq.client.list_projects()
         kbc_project_id = self._get_kbc_project_id()
         # for secure we need to filter only current projects by prefix
@@ -228,7 +228,7 @@ class Component(ComponentBase):
         Returns:
 
         """
-        bq = BigqueryClient(credentials=self.get_bigquery_credentials(), location=self.location)
+        bq = BigqueryClient(credentials=self.get_bigquery_credentials())
         projects = bq.client.list_projects()
         kbc_project_id = self._get_kbc_project_id()
         # for secure we need to filter only current projects by prefix
@@ -247,7 +247,7 @@ class Component(ComponentBase):
         bq_project = self.configuration.parameters.get(KEY_DESTINATION_PROJECT_ID)
         if not bq_project:
             raise UserException('No project selected.')
-        bq = BigqueryClient(credentials=self.get_bigquery_credentials(), location=self.location)
+        bq = BigqueryClient(credentials=self.get_bigquery_credentials())
         return [SelectElement(value=d.dataset_id, label=f'{d.dataset_id}') for d in bq.client.list_datasets(bq_project)]
 
 